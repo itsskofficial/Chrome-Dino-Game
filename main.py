cdcfa3820f586792f632da86a17d1bf7b924eb9f
SK <sarthakkarandikar03@gmail.com>
2022-11-05 04:20:16 +0000

    Nov 5, 2022, 9:50 AM

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 from time import *
 import pyautogui
+import PIL
 
 service = Service(executable_path="D:/Skills/Python/Projects/Google Meet Bot/ChromeDrivers/win32")
 driver = webdriver.Chrome(service=service)
 
 driver.get('https://chromedino.com/')
 sleep(2)
-p
+pyautogui.
 while True:
 
 
