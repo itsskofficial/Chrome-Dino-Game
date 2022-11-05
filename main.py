ee477ffad4b2914fd9e852b7b3b9b619cb3f86dd
SK <sarthakkarandikar03@gmail.com>
2022-11-05 04:59:47 +0000

    Nov 5, 2022, 10:29 AM

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 from time import *
 import pyautogui
-from PIL import ImageGrab
+from PIL import ImageGrab,Image
 
 service = Service(executable_path="D:/Skills/Python/Projects/Google Meet Bot/ChromeDrivers/win32/chromedriver.exe")
 driver = webdriver.Chrome(service=service)
@@ -15,7 +15,7 @@ t=30
 while t:
     game=driver.find_element_by_class_name('runner-container')
     game.screenshot('screen.png')
-    
+    img=Image.Ope
     sleep(1)
     t-=1
 
